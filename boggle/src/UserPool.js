a7aae8375c93403c1f7f47c4e9ff22c3234790ee
Meghna Kumar <meghna.kumar@dal.ca>
2022-07-26 16:49:17 -0300

    Update cognito user pool details

--- boggle/src/UserPool.js
+++ boggle/src/UserPool.js
@@ -1,8 +1,8 @@
 import {CognitoUserPool} from "amazon-cognito-identity-js";
 
 const poolData = {
-    UserPoolId: "us-east-1_2cKW1SX9e",
-    ClientId: "43sle14hef0f71hhfgm65c0fca"
+    UserPoolId: "us-east-1_qBFz0VdnL",
+    ClientId: "17ku5qs1kbtrqpm3p1qmjee59"
 }
 
 export default new CognitoUserPool(poolData);
